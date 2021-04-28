e9a17491434ddf2e46efb1b382a0afb04e8ada9f
朱佳宝 <zhujiabao3@jd.com>
2021-04-28 21:15:37 +0800

    push

---
+++ lib/TearDownPlugin.rb
@@ -0,0 +1,12 @@
+require "TearDownPlugin/version"
+
+module TearDownPlugin
+  class Error < StandardError; end
+  
+  def self.hi
+      puts "Hello world!"
+  end
+    
+   
+end
+
---
+++ lib/TearDownPlugin/version.rb
@@ -0,0 +1,3 @@
+module TearDownPlugin
+  VERSION = "0.1.0"
+end
